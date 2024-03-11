e3ef92cf9324095d8a5e5b2ad8a3a3e4b2dae5b5
bkvdm <vadim@991199.ru>
2024-03-11 13:36:17 +0300

    Курсовая работа №3 (телеграмм бот - напоминание о событии): https://skyengpublic.notion.site/Telegram-11722f88f0a04c3b8a2ac63a0c4eb6e2

---
+++ src/main/java/tel/bvm/telegrambot/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,22 @@
+package tel.bvm.telegrambot.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.DeleteMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tel.bvm.telegrambot.TelegramBotApplication;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+}
---
+++ src/main/java/tel/bvm/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,36 @@
+package tel.bvm.telegrambot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.pengrad.telegrambot.model.Update;
+
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);}
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+        } );
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
